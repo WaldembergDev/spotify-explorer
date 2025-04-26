e02b4c88e0ab442b0ae1c6b968269970b82eaadd
waldembergdev <waldemberg.pereira@outlook.com>
2025-04-26 20:46:42 -0300

    Desenvolvido as funções save_token e get_token_valid da view authorization_spotify

--- authorization_spotify/views.py
+++ authorization_spotify/views.py
@@ -1,8 +1,13 @@
+import datetime
 from django.shortcuts import render
 import requests
 from dotenv import load_dotenv
 from pathlib import Path
 import os
+from .models import Token
+from datetime import date, datetime
+from datetime import timedelta
+from django.utils import timezone
 
 load_dotenv(Path('.venv/.env'))
 
@@ -34,4 +39,27 @@ def get_token():
         print(f'Erro ao fazer a requisição: {e}')
         return None
 
-    
+
+def save_token():
+    data_token = get_token()
+    if not data_token:
+        return False
+    # pegando a hora atual, adicionando a validade do token e subtraindo 10 segundos
+    expires_in = timezone.now() + timedelta(seconds = data_token.get('expires_in')) - timedelta(seconds = 10)
+    token = Token(
+        access_token = data_token.get('access_token'),
+        token_type = data_token.get('token_type'),
+        expires_in = expires_in
+    )
+    token.save()
+    return token
+
+def get_token_valid():
+    last_token = Token.objects.last()
+
+    # verificando se agora é maior do que a validade do último token gerado
+    if timezone.now() > last_token.expires_in:
+        new_token = save_token()
+        return new_token.access_token        
+    else:
+        return last_token.access_token
