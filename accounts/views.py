0fda230609b8ce14aa05f4611aa6bd45c76c2167
waldembergdev <waldemberg.pereira@outlook.com>
2025-04-27 18:44:59 -0300

    Desenvolvido o template register

--- accounts/views.py
+++ accounts/views.py
@@ -18,4 +18,25 @@ def login(request):
         else:
             messages.add_message(request, constants.WARNING, message='E-mail ou senha inválidos!')
             return render(request, 'login.html')
+
+def register(request):
+    if request.method == 'GET':
+        return render(request, 'register.html')
+    else:
+        first_name = request.POST.get('first_name')
+        surname = request.POST.get('surname')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm_password')
+        if password != confirm_password:
+            pass
+        user = CustomUser(
+            first_name = first_name,
+            surname = surname,
+            email = email,
+            password = password,
+        )
+        user.save()
+        messages.add_message(request, constants.SUCCESS, 'Conta criada com sucesso!')
+        return redirect('/accounts/register')
             
