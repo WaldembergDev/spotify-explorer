8efa1e3ff6b7f8268df5e6425bcf901ddf300bba
waldembergdev <waldemberg.pereira@outlook.com>
2025-04-27 17:28:06 -0300

    Desenvolvido o template login

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import CustomUser
+from django.contrib import auth
+from django.contrib.messages import constants
+from django.contrib import messages
 
 # Create your views here.
 def login(request):
-    return render(request, 'login.html')
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    else:
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = auth.authenticate(request, email = email, password = password)
+        if user:
+            auth.login(request, user)
+            redirect('/plataform/home')
+        else:
+            messages.add_message(request, constants.WARNING, message='E-mail ou senha inválidos!')
+            return render(request, 'login.html')
+            
--- artist_hits_finder/settings.py
+++ artist_hits_finder/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,3 +129,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # definindo o user
 AUTH_USER_MODEL = 'accounts.CustomUser'
+
+# messages
+MESSAGE_TAGS = {
+    constants.DEBUG: 'alert-primary',
+    constants.ERROR: 'alert-danger',
+    constants.INFO: 'alert-info',
+    constants.SUCCESS: 'alert-success',
+    constants.WARNING: 'alert-warning'
+}
