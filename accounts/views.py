f3c50c87ca7f3bb2f4b9ff93b82fbaf1e563f578
waldembergdev <waldemberg.pereira@outlook.com>
2025-04-27 20:11:03 -0300

    Criado as validações dos dados para as funções profile e register

--- accounts/views.py
+++ accounts/views.py
@@ -39,15 +39,33 @@ def register(request):
         email = request.POST.get('email')
         password = request.POST.get('password')
         confirm_password = request.POST.get('confirm_password')
+        # fazendo as validações
+        if not first_name:
+            messages.add_message(request, constants.WARNING, 'O nome não pode ser vazio!')
+            return redirect('/accounts/register')
+        if not surname:
+            messages.add_message(request, constants.WARNING, 'O sobrenome não pode ser vazio!')
+            return redirect('/accounts/register')
+        if not email:
+            messages.add_message(request, constants.WARNING, 'O email não pode ser vazio!')
+            return redirect('/accounts/register')
         if password != confirm_password:
-            pass
-        user = CustomUser.objects.create_user(
+            messages.add_message(request, constants.WARNING, 'As senhas informadas não são iguais!')
+            return redirect('/accounts/register')
+        # verificando se existe algum usuário cadastrado com o e-mail
+        user = CustomUser.objects.filter(email = email)
+        if user:
+            messages.add_message(request, constants.WARNING, 'Já existe um usuário cadastrado com esse e-mail!')
+            return redirect('/accounts/register')
+        # criando um novo usuário
+        new_user = CustomUser.objects.create_user(
             first_name = first_name,
             surname = surname,
             email = email,
             password = password,
         )
-        user.save()
+        # salvando o usuário no banco de dados
+        new_user.save()
         messages.add_message(request, constants.SUCCESS, 'Conta criada com sucesso!')
         return redirect('/accounts/register')
 
@@ -66,6 +84,16 @@ def profile(request):
         email = request.POST.get('email')
         password = request.POST.get('password')
         confirm_password = request.POST.get('password')
+        # fazendo as validações
+        if not first_name:
+            messages.add_message(request, constants.WARNING, 'O nome não pode ser vazio!')
+            return redirect('/accounts/register')
+        if not surname:
+            messages.add_message(request, constants.WARNING, 'O sobrenome não pode ser vazio!')
+            return redirect('/accounts/register')
+        if not email:
+            messages.add_message(request, constants.WARNING, 'O email não pode ser vazio!')
+            return redirect('/accounts/register')
         if password != confirm_password:
             messages.add_message(request, constants.WARNING, 'As senhas informadas não são iguais!')
             return redirect('/account/profile')
