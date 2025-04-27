9731b436269dd6849f4178c9bb78da191f8f4c81
waldembergdev <waldemberg.pereira@outlook.com>
2025-04-27 20:01:50 -0300

    Desenvolvido o template profile

--- accounts/views.py
+++ accounts/views.py
@@ -29,6 +29,7 @@ def login(request):
             messages.add_message(request, constants.WARNING, message='E-mail ou senha inválidos!')
             return render(request, 'login.html')
 
+
 def register(request):
     if request.method == 'GET':
         return render(request, 'register.html')
@@ -50,6 +51,33 @@ def register(request):
         messages.add_message(request, constants.SUCCESS, 'Conta criada com sucesso!')
         return redirect('/accounts/register')
 
+
 def logout(request):
     auth.logout(request)
     return redirect('/accounts/login')
+
+
+def profile(request):
+    if request.method == 'GET':
+        return render(request, 'profile.html')
+    else:
+        first_name = request.POST.get('first_name')
+        surname = request.POST.get('surname')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('password')
+        if password != confirm_password:
+            messages.add_message(request, constants.WARNING, 'As senhas informadas não são iguais!')
+            return redirect('/account/profile')
+        # obtendo o usuário logado
+        user = request.user
+        # alterando os dados do usuário logado
+        user.first_name = first_name
+        user.surname = surname
+        user.email = email
+        user.set_password(password)
+        user.save()
+        # mantendo o usuário logado
+        auth.update_session_auth_hash(request, user)
+        # redirecionando o usuário para o template profile
+        return redirect('/accounts/profile')
